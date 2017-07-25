185af398bb0c3c920b58397b97f70bb6c85efe90
camidgek <kevincamidge@gmail.com>
2017-07-25 15:54:31 -0400

    Minor changes.

--- LiftSAFE/LiftSAFE/ApplicationMain.cpp
+++ LiftSAFE/LiftSAFE/ApplicationMain.cpp
@@ -46,35 +46,8 @@ int CApplication::Run(HINSTANCE hInstance, int nCmdShow)
 	RGBQUAD* pInfraredImage = new RGBQUAD[irHeight * irWidth];
 	RGBQUAD* pOriginal = pInfraredImage;
 
-
-	WNDCLASS  wc;
-
-	// Dialog custom window class
-	ZeroMemory(&wc, sizeof(wc));
-	wc.style = CS_HREDRAW | CS_VREDRAW;
-	wc.cbWndExtra = DLGWINDOWEXTRA;
-	wc.hCursor = LoadCursorW(NULL, IDC_ARROW);
-	wc.hIcon = LoadIconW(hInstance, MAKEINTRESOURCE(IDI_APP));
-	wc.lpfnWndProc = DefDlgProcW;
-	wc.lpszClassName = L"InfraredBasicsAppDlgWndClass";
-
-	if (!RegisterClassW(&wc))
-	{
-		return 0;
-	}
-
-	// Create main application window
-	HWND hWndApp = CreateDialogParamW(
-		NULL,
-		MAKEINTRESOURCE(IDD_APP),
-		NULL,
-		(DLGPROC)CImageGrabber::MessageRouter,
-		reinterpret_cast<LPARAM>(this));
-
-	// Show window
-	ShowWindow(hWndApp, nCmdShow);
-
 	MSG       msg = { 0 };
+	/*
 	// Main message loop
 	while (msg.message != WM_QUIT)
 	{
@@ -88,7 +61,7 @@ int CApplication::Run(HINSTANCE hInstance, int nCmdShow)
 			DispatchMessageW(&msg);
 		}
 	}
-
+	*/
 	return static_cast<int>(msg.wParam);
 }
 
@@ -99,7 +72,7 @@ void connectArduino()
 	char *port_name = "\\\\.\\COM20";
 
 	//String for incoming data
-	char incomingData[MAX_DATA_LENGTH];
+	//char incomingData[MAX_DATA_LENGTH];
 
 	SerialPort arduino(port_name);
 	char output[MAX_DATA_LENGTH];
